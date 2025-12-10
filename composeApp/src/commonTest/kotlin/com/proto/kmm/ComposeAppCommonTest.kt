0c28cf4c306859a7cab77d5f2b895d35bc3d038b
Dharmesh <50448775+dhameliyadharmesh@users.noreply.github.com>
2025-12-10 22:15:27 +0530

    initial commit

---
+++ composeApp/src/commonTest/kotlin/com/proto/kmm/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.proto.kmm
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/proto/kmm/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.proto.kmm
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
